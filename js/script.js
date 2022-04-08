af1bce6dfcea1655403808d97e52244eaf95f60f
SimoneCorona <corona.simone93@gmail.com>
2022-04-08 09:58:24 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 // va applicato uno sconto del 40% per gli over 65.
 
 
-
+// ELABORAZIONE
 // Chiedere all'utente il numero di chilometri che vuole percorrere
 const num_km = parseInt(prompt('Quanti Km devi percorrere?'))
 console.log(num_km);
@@ -16,7 +16,7 @@ const user_age = parseInt(prompt('Quanti anni ha il passeggero?'))
 console.log(user_age);
 
 //prezzo del biglietto definito in base ai km (0.21 € al km)
-const price_per_km = (num_km * 0.21)
+let price_per_km = (num_km * 0.21)
 console.log(price_per_km);
 
 
@@ -25,17 +25,17 @@ console.log(price_per_km_rounded);
 
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
-let price_discount;
+let final_price;
 if (user_age < 18) {
-    price_discount = (price_per_km_rounded * 20) / 100;
-} else if(user_age > 65) {
-    price_discount = (price_per_km_rounded * 40) / 100;
+    price_discount = (price_per_km_rounded * 20 / 100);
+    final_price = (price_per_km_rounded - price_discount).toFixed(2);
+} else if (user_age > 65) {
+    price_discount = (price_per_km_rounded * 40 / 100);
+    final_price = (price_per_km_rounded - price_discount).toFixed(2);
 } else {
-    price_discount = price_per_km_rounded;
+    final_price = parseInt(price_per_km_rounded);
 }
-console.log(price_discount);
-
-const final_price = price_per_km - price_discount;
 console.log(final_price);
- 
+
+//OUTPUT
 document.getElementById("price_travel").innerHTML = final_price;
